baa53f41100e612ed6d61eea5b630d745b37f95c
Curtis <curtischang2510@gmail.com>
2024-12-03 17:15:19 +0800

    Add function to convert transaction to from source to target currency

--- src/services/HandleCurrencyService.jsx
+++ src/services/HandleCurrencyService.jsx
@@ -1,3 +1,8 @@
+import Freecurrencyapi from "@everapi/freecurrencyapi-js";
+
+const baseCurrency = "SGD"; // hard coded for now
+const freecurrencyapi = new Freecurrencyapi("YOUR-API-KEY");
+
 const currencyOptions = [
   { value: "USD", label: "USD - United States Dollar" },
   { value: "EUR", label: "EUR - Euro" },
@@ -6,4 +11,37 @@ const currencyOptions = [
   { value: "SGD", label: "SGD - Singapore Dollar" },
 ];
 
-export { currencyOptions }
+const convertToCurrency = (srcAmount, sourceCurrency, targetCurrency) => {
+  return freecurrencyapi
+    .latest({
+      base_currency: sourceCurrency,
+      currencies: targetCurrency,
+    })
+    .then((response) => {
+      console.log(response);
+
+      const conversionRate = response.data.targetCurrency;
+      if (conversionRate) {
+        return srcAmount * conversionRate;
+      } else {
+        console.log('Conversion rate not available');
+        return null;
+      }
+    })
+    .catch((error) => {
+      console.error("Error during API call:", error);
+      return null; // Provide a fallback value in case of an error
+    })
+    .finally(() => {
+      console.log("API call completed."); // Always executes, regardless of success or failure
+    });
+};
+
+const convertToBaseCurrency = (srcAmount, sourceCurrency) => {
+  if (sourceCurrency == baseCurrency) {
+    return Promise.resolve(srcAmount);
+  }
+  return convertToCurrency(srcAmount, sourceCurrency, baseCurrency);
+}
+
+export { currencyOptions, convertToCurrency, convertToBaseCurrency };
